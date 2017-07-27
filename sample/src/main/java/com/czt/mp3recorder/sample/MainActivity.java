ffd73107a1846613d0509f14fbe42a2d9e8b9242
heliangwei <heliangwei@hujiang.com>
2017-07-27 15:03:16 +0800

    update: play audio in demo

--- sample/src/main/java/com/czt/mp3recorder/sample/MainActivity.java
+++ sample/src/main/java/com/czt/mp3recorder/sample/MainActivity.java
@@ -5,17 +5,21 @@ import java.io.File;
 import java.io.IOException;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.czt.mp3recorder.MP3Recorder;
 
 public class MainActivity extends Activity {
-
-	private MP3Recorder mRecorder = new MP3Recorder(new File(Environment.getExternalStorageDirectory(),"test.mp3"));
+	private File mRecordFile = new File(Environment.getExternalStorageDirectory(),"test.mp3");
+	private MediaPlayer mMediaPlayer;
+	private MP3Recorder mRecorder = new MP3Recorder(mRecordFile);
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -37,6 +41,27 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				mRecorder.stop();
+			}
+		});
+		Button playButton = (Button) findViewById(R.id.PlayButton);
+		playButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (mRecordFile.exists()){
+					try {
+						mMediaPlayer = MediaPlayer.create(MainActivity.this, Uri.fromFile(mRecordFile));
+						mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+							@Override
+							public void onPrepared(MediaPlayer mp) {
+								mMediaPlayer.start();
+							}
+						});
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				} else {
+					Toast.makeText(MainActivity.this, "录音文件不存在", Toast.LENGTH_SHORT).show();
+				}
 			}
 		});
 	}
